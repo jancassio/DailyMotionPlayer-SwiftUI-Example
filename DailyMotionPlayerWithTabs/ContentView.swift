d2b0bdba6ed55ec8d12e0889231800223780f76b
Jan Cassio <hey@jancassio.com>
2021-07-20 10:28:42 -0400

    Initial Commit

---
+++ DailyMotionPlayerWithTabs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DailyMotionPlayerWithTabs
+//
+//  Created by Jan Cassio on 2021-07-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DailyMotionPlayerWithTabs/DailyMotionPlayerWithTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMotionPlayerWithTabsApp.swift
+//  DailyMotionPlayerWithTabs
+//
+//  Created by Jan Cassio on 2021-07-20.
+//
+
+import SwiftUI
+
+@main
+struct DailyMotionPlayerWithTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
